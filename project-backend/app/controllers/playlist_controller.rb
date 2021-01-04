567e7ac88f7b82efb8a4f0cdd1f4febc1d2bcb0e
brockbyrd <brockbyrdd@gmail.com>
2021-01-04 12:34:20 -0600

    "resets migrations and controllers with scaffold g"

--- project-backend/app/controllers/playlist_controller.rb
+++
@@ -1,3 +0,0 @@
-class PlaylistController < ApplicationController
-    
-end
--- project-backend/db/schema.rb
+++ project-backend/db/schema.rb
@@ -10,22 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_175638) do
-
-  create_table "playlists", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "songs", force: :cascade do |t|
-    t.string "name"
-    t.string "artist"
-    t.string "genre"
-    t.integer "playlist_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
