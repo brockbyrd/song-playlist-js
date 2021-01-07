26dde1c4ac0d2250d7ff967147cdb109d84a43e4
brockbyrd <brockbyrdd@gmail.com>
2021-01-07 11:54:03 -0600

    "passes in all the song data to playlistSongHTML"

--- project-frontend/src/song.js
+++ project-frontend/src/song.js
@@ -10,7 +10,9 @@ class Song{
 
     playlistSongHTML = function() {
         return `<div class="card" song-id="${this.id}">
-                <h3><strong class="song-name">${this.name}</strong></h3>
+                <h3><strong class="song-name">${this.name} by: </strong></h3>
+                <h3><strong class="song-name">${this.artist}</strong></h3>
+                <h3><strong class="song-name">${this.genre}</strong></h3>
                 <button class="delete-song-button">Delete Song</button> <br>
                 </div>
         `
