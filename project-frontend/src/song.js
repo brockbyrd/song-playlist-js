da59d8e9b0416dbbc633442dd7e6352158a5952e
brockbyrd <brockbyrdd@gmail.com>
2021-01-07 10:44:25 -0600

    "sets up fetch post request to add a song"

--- project-frontend/src/song.js
+++ project-frontend/src/song.js
@@ -1,4 +1,4 @@
-const SONGS_URL = 'http://127.0.0.1:3001/songs'
+const SONG_URL = 'http://127.0.0.1:3001/songs'
 
 class Song{
     constructor(data){
@@ -7,4 +7,25 @@ class Song{
         this.artist = data.artist
         this.genre = data.genre
     }
+}
+
+function addSong(){
+    const song = {
+        name: document.getElementById('name').value,
+        artist: document.getElementById('artist').value,
+        genre: document.getElementById('genre').value
+    }
+
+    fetch(SONG_URL, {
+        method: 'POST',
+        body: JSON.stringify(song),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(resp => resp.json())
+    .then(song => {
+        console.log(song)
+        getSongs()
+    })
 }
