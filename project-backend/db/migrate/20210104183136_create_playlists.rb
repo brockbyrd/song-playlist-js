ba7679c930facef2ace3d020116ed96b06cbfa8d
brockbyrd <brockbyrdd@gmail.com>
2021-01-08 10:39:11 -0600

    "creates seed data"

--- project-backend/db/migrate/20210104183136_create_playlists.rb
+++ project-backend/db/migrate/20210104183136_create_playlists.rb
@@ -2,7 +2,6 @@ class CreatePlaylists < ActiveRecord::Migration[6.1]
   def change
     create_table :playlists do |t|
       t.string :name
-      t.string :description
 
       t.timestamps
     end
--- project-backend/db/seeds.rb
+++ project-backend/db/seeds.rb
@@ -6,8 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Playlist.create(name: 'Good Songs', description: 'Songs to listen to for a good time!')
+Playlist.create(name: 'Good Songs')
+Playlist.create(name: 'Dance Songs')
+Playlist.create(name: "Rock \'n Roll!!")
 
 Song.create(name: 'Good Life', artist: 'Kanye West', genre: 'Rap', playlist_id: 1)
 Song.create(name: 'Bartender', artist: 'T-Pain', genre: 'Hip-Hop', playlist_id: 1)
 Song.create(name: 'Tampa', artist: 'Cicoeee P', genre: 'Rap', playlist_id: 1)
+
+Song.create(name: 'One Dance', artist: 'Drake', genre: 'Hip-Hop', playlist_id: 2)
+Song.create(name: 'September', artist: 'Earth, Wind, & Fire', genre: 'Disco', playlist_id: 2)
+Song.create(name: "Don\'t Stop \'til You Get Enough", artist: "Michael Jackson", genre: 'Pop', playlist_id: 2)
+
+Song.create(name: "Sweet Child O\' Mine", artist: "Guns N\' Roses", genre: "Classic Rock", playlist_id: 3)
+Song.create(name: "Sweet Home Alabama", artist: "Lynyrd Skynrd", genre: "Classic Rock", playlist_id: 3)
+Song.create(name: "Blitzkrieg Bop", artist: "Ramones", genre: "Punk Rock", playlist_id: 3)
